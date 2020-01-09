8157103532f3160426949184670964dd05b14761
tuannx21 <tuannguyen@DESKTOP-1E1AV4K.localdomain>
2020-01-09 22:53:51 +0700

    update

--- src/sagas/api.js
+++ src/sagas/api.js
@@ -10,14 +10,42 @@ const getResponseData = response => response.data
 
 const Article = {
   findAll: () => axios.get(`${API_ROOT}/articles`).then(getResponseData),
-  findBySlug: slug => axios.get(`${API_ROOT}/articles/${slug}`).then(getResponseData)
+  findAllFeed: (params = {}) => axios.get(`/articles/feed`, { params }).then(getResponseData),
+  findBySlug: slug => axios.get(`/articles/${slug}`).then(getResponseData),
+  create: article => axios.post(`/articles`, article).then(getResponseData),
+  update: (slug, article) => axios.put(`/articles/${slug}`, article).then(getResponseData),
+  delete: slug => axios.delete(`/articles/${slug}`),
+  favorite: slug => axios.post(`/articles/${slug}/favorite`).then(getResponseData),
+  unfavorite: slug => axios.delete(`/articles/${slug}/favorite`).then(getResponseData),
 }
 
 const Tag = {
-  getAll: () => axios.get(`${API_ROOT}/tags`).then(getResponseData)
+  getAll: () => axios.get(`/tags`).then(getResponseData)
+}
+
+const Auth = {
+  login: user => axios.post(`/users/login`, user).then(getResponseData),
+  register: user => axios.post(`/users`, user).then(getResponseData)
+}
+
+const User = {
+  getCurrent: () => axios.get(`/user`).then(getResponseData),
+  update: user => axios.put(`/user`, user).then(getResponseData),
+  getProfile: username => axios.get(`/user/${username}`).then(getResponseData),
+  follow: username => axios.post(`/user/${username}/follow`).then(getResponseData),
+  unfollow: username => axios.delete(`/user/${username}/follow`).then(getResponseData)
+}
+
+const Comment = {
+  findAll: slug => axios.get(`/articles/${slug}/comments`).then(getResponseData),
+  create: (slug, comment) => axios.post(`/articles/${slug}/comments`, comment).then(getResponseData),
+  delete: (slug, commentId) => axios.delete(`/articles/${slug}/comments/${commentId}`)
 }
 
 export {
   Article,
-  Tag
+  Tag,
+  Auth,
+  User,
+  Comment
 }
