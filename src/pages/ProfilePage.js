c77933ea0242b1b043b8e5785c74b5dfb6e0f5e3
tuannx21 <tuannguyen@DESKTOP-1E1AV4K.localdomain>
2020-02-16 22:25:48 +0700

    update

--- src/pages/ProfilePage.js
+++ src/pages/ProfilePage.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react'
+import React, { useEffect } from 'react'
 import { connect } from 'react-redux'
 import { GET_PROFILE_START, FOLLOW_PROFILE_START, UNFOLLOW_PROFILE_START, CLEAR_PROFILE, FETCH_ARTICLES_START } from '../store/constant'
 import ArticleList from '../component/ArticleList'
@@ -7,6 +7,7 @@ import { NavLink } from 'react-router-dom'
 const mapStateToProps = state => ({
   currentProfile: state.user.profile,
   currentUser: state.auth.currentUser,
+  location: state.router.location
 })
 
 const mapDispatchToProps = dispatch => ({
@@ -17,71 +18,61 @@ const mapDispatchToProps = dispatch => ({
   onUnload: () => dispatch({ type: CLEAR_PROFILE })
 })
 
-class ProfilePage extends Component {
-  componentDidMount() {
-    const { match, getProfile, fetchArticles } = this.props
+const ProfilePage = props => {
+  const { getProfile, fetchArticles, onUnload, currentProfile, currentUser, match, location } = props
 
+  useEffect(() => {
     getProfile(match.params.username)
-    match.path.includes('favorited') ? fetchArticles({ favorited: match.params.username }) : fetchArticles({ author: match.params.username })
+
+    match.path.includes('favorited')
+      ? fetchArticles({ ...location.query, favorited: match.params.username })
+      : fetchArticles({ ...location.query, author: match.params.username })
+
+    return onUnload
+  }, [match, location, getProfile, fetchArticles, onUnload])
+
+  const followUserButton = () => {
+    if (currentUser.username === currentProfile.username) return null
+    return currentProfile.following
+      ? <button className="btn btn-sm btn-outline-secondary action-btn" onClick={() => props.unfollowProfile(currentProfile.username)}><i className="ion-minus-round"></i>&nbsp;Unfollow {currentProfile.username}</button>
+      : <button className="btn btn-sm btn-outline-secondary action-btn" onClick={() => props.followProfile(currentProfile.username)}><i className="ion-plus-round"></i>&nbsp;Follow {currentProfile.username}</button>
   }
 
-  componentWillUnmount() {
-    this.props.onUnload()
-  }
-
-  componentDidUpdate(prevProps) {
-    const { match, fetchArticles } = this.props
-
-    if (prevProps.match.path !== match.path) {
-      match.path.includes('favorited') ? fetchArticles({ favorited: match.params.username }) : fetchArticles({ author: match.params.username })
-    }
-  }
-
-  render() {
-    const { currentProfile, currentUser } = this.props
-
-    const followUserButton = () => {
-      if (currentUser.username === currentProfile.username) return null
-      return currentProfile.following
-        ? <button className="btn btn-sm btn-outline-secondary action-btn" onClick={() => this.props.unfollowProfile(currentProfile.username)}><i className="ion-minus-round"></i>&nbsp;Unfollow {currentProfile.username}</button>
-        : <button className="btn btn-sm btn-outline-secondary action-btn" onClick={() => this.props.followProfile(currentProfile.username)}><i className="ion-plus-round"></i>&nbsp;Follow {currentProfile.username}</button>
-    }
-    return (
-      <div className="profile-page">
-        <div className="user-info">
-          <div className="container">
-            <div className="row">
-              <div className="col-xs-12 col-md-10 offset-md-1">
-                <img src={currentProfile.image} className="user-img" alt="avatar" />
-                <h4>{currentProfile.username}</h4>
-                <p>{currentProfile.bio}</p>
-                {followUserButton()}
-              </div>
-            </div>
-          </div>
-        </div>
-
+  return (
+    <div className="profile-page">
+      <div className="user-info">
         <div className="container">
           <div className="row">
-
             <div className="col-xs-12 col-md-10 offset-md-1">
-              <div className="articles-toggle">
-                <ul className="nav nav-pills outline-active">
-                  <li className="nav-item">
-                    <NavLink exact to={`/user/@${currentProfile.username}`} className="nav-link">My Articles</NavLink>
-                  </li>
-                  <li className="nav-item">
-                    <NavLink exact to={`/user/@${currentProfile.username}/favorited`} className="nav-link">Favorited Articles</NavLink>
-                  </li>
-                </ul>
-              </div>
-              <ArticleList />
+              <img src={currentProfile.image} className="user-img" alt="avatar" />
+              <h4>{currentProfile.username}</h4>
+              <p>{currentProfile.bio}</p>
+              {followUserButton()}
             </div>
           </div>
         </div>
       </div>
-    )
-  }
+
+      <div className="container">
+        <div className="row">
+
+          <div className="col-xs-12 col-md-10 offset-md-1">
+            <div className="articles-toggle">
+              <ul className="nav nav-pills outline-active">
+                <li className="nav-item">
+                  <NavLink exact to={`/user/@${currentProfile.username}`} className="nav-link">My Articles</NavLink>
+                </li>
+                <li className="nav-item">
+                  <NavLink exact to={`/user/@${currentProfile.username}/favorited`} className="nav-link">Favorited Articles</NavLink>
+                </li>
+              </ul>
+            </div>
+            <ArticleList />
+          </div>
+        </div>
+      </div>
+    </div>
+  )
 }
 
 export default connect(mapStateToProps, mapDispatchToProps)(ProfilePage) 
